3760c0eda919c69030146524d21e0caeac00feb4
John Ed Quinn <johqunn@amazon.com>
2023-04-18 10:20:00 -0700

    Adds Lexer benchmarks

--- examples/partiql/src/jmh/kotlin/io/johnedquinn/partiql/parser/PartiQLParserBenchmark.kt
+++ examples/partiql/src/jmh/kotlin/io/johnedquinn/partiql/parser/PartiQLParserBenchmark.kt
@@ -1,8 +1,10 @@
 package io.johnedquinn.partiql.parser
 
 import com.amazon.ionelement.api.createIonElementLoader
+import io.johnedquinn.kanonic.runtime.parse.KanonicLexer
 import io.johnedquinn.kanonic.runtime.parse.KanonicParser
 import io.johnedquinn.partiql.generated.PartiQLSpecification
+import org.antlr.v4.runtime.CharStreams
 import org.openjdk.jmh.annotations.Benchmark
 import org.openjdk.jmh.annotations.BenchmarkMode
 import org.openjdk.jmh.annotations.Fork
@@ -15,6 +17,7 @@ import org.openjdk.jmh.annotations.State
 import org.openjdk.jmh.annotations.Warmup
 import org.openjdk.jmh.infra.Blackhole
 import org.partiql.lang.syntax.PartiQLParserBuilder
+import org.partiql.lang.syntax.antlr.PartiQLTokens
 import java.util.concurrent.TimeUnit
 
 
@@ -24,9 +27,9 @@ import java.util.concurrent.TimeUnit
 internal open class PartiQLParserBenchmark {
     companion object {
         private const val FORK_VALUE: Int = 1 // 2
-        private const val MEASUREMENT_ITERATION_VALUE: Int = 5 // 10
+        private const val MEASUREMENT_ITERATION_VALUE: Int = 10 // 10
         private const val MEASUREMENT_TIME_VALUE: Int = 1
-        private const val WARMUP_ITERATION_VALUE: Int = 3 // 5
+        private const val WARMUP_ITERATION_VALUE: Int = 5 // 5
         private const val WARMUP_TIME_VALUE: Int = 1
     }
 
@@ -34,6 +37,7 @@ internal open class PartiQLParserBenchmark {
     open class MyState {
         val parser = PartiQLParser
         val kanonicParser = KanonicParser.Builder.standard().withSpecification(PartiQLSpecification).build()
+        val kanonicLexer = KanonicLexer.Builder.standard().withDefinitions(PartiQLSpecification.grammar.tokens).build()
         val partiQLParser = PartiQLParserBuilder.standard().build()
 
         @Param(
@@ -42,12 +46,6 @@ internal open class PartiQLParserBenchmark {
             "SELECT (SELECT a FROM (SELECT a FROM b)) FROM (SELECT (SELECT a FROM b) FROM c)"
         )
         public var inputIndex: String = ""
-
-        var inputs = arrayOf<String>(
-            "SELECT a FROM b",
-            "SELECT (SELECT a FROM b) FROM b",
-            "SELECT (SELECT a FROM (SELECT a FROM b)) FROM (SELECT (SELECT a FROM b) FROM c)"
-        )
     }
 
 //    @Benchmark
@@ -59,6 +57,28 @@ internal open class PartiQLParserBenchmark {
 //        val expr = state.kanonicParser.parse(state.inputIndex)
 //        blackhole.consume(expr)
 //    }
+
+    @Benchmark
+    @Fork(value = FORK_VALUE)
+    @Measurement(iterations = MEASUREMENT_ITERATION_VALUE, time = MEASUREMENT_TIME_VALUE)
+    @Warmup(iterations = WARMUP_ITERATION_VALUE, time = WARMUP_TIME_VALUE)
+    @Suppress("UNUSED")
+    fun tokenizeUsingNewLexer(state: MyState, blackhole: Blackhole) {
+        state.kanonicLexer.tokenize(state.inputIndex).forEach {
+            blackhole.consume(it)
+        }
+    }
+
+    @Benchmark
+    @Fork(value = FORK_VALUE)
+    @Measurement(iterations = MEASUREMENT_ITERATION_VALUE, time = MEASUREMENT_TIME_VALUE)
+    @Warmup(iterations = WARMUP_ITERATION_VALUE, time = WARMUP_TIME_VALUE)
+    @Suppress("UNUSED")
+    fun tokenizeUsingAntlrLexer(state: MyState, blackhole: Blackhole) {
+        PartiQLTokens(CharStreams.fromString(state.inputIndex)).allTokens.forEach {
+            blackhole.consume(it)
+        }
+    }
 
     @Benchmark
     @Fork(value = FORK_VALUE)
--- examples/partiql/src/test/kotlin/io/johnedquinn/partiql/parser/PartiQLParserTests.kt
+++ examples/partiql/src/test/kotlin/io/johnedquinn/partiql/parser/PartiQLParserTests.kt
@@ -1,10 +1,14 @@
 package io.johnedquinn.partiql.parser
 
+import io.johnedquinn.kanonic.runtime.parse.KanonicLexer
+import io.johnedquinn.partiql.generated.PartiQLSpecification
 import org.junit.jupiter.api.Test
 import org.partiql.lang.domains.PartiqlAst
+import java.time.Instant
 import kotlin.test.assertEquals
 
 class PartiQLParserTests {
+    private val lexer = KanonicLexer.Builder.standard().withDefinitions(PartiQLSpecification.grammar.tokens).build()
     private val parser = PartiQLParser
 
     @Test
@@ -21,6 +25,7 @@ class PartiQLParserTests {
         val result = parser.parseAstStatement(query)
         assertEquals(expected, result)
     }
+
     @Test
     fun test2() {
         val query = "SELECT (SELECT a FROM b) FROM b"
@@ -30,6 +35,25 @@ class PartiQLParserTests {
     @Test
     fun test3() {
         val query = "SELECT (SELECT a FROM (SELECT a FROM b)) FROM (SELECT (SELECT a FROM b) FROM c)"
+        val start = Instant.now()
         parser.parseAstStatement(query)
+        val end = Instant.now()
+        val total = timeDiff(start, end)
+        println("PARSED IN $total")
+    }
+
+    @Test
+    fun test4() {
+        val query = "SELECT (SELECT a FROM (SELECT a FROM b)) FROM (SELECT (SELECT a FROM b) FROM c)"
+        val start = Instant.now()
+        lexer.tokenize(query).toList()
+        val end = Instant.now()
+        val total = timeDiff(start, end)
+        println("TOKENIZED IN $total")
+    }
+
+    public fun timeDiff(older: Instant, newer: Instant): String {
+        val secondsInNanos = (newer.epochSecond - older.epochSecond) * 1_000_000_000L + newer.nano - older.nano
+        return "$secondsInNanos ns"
     }
 }
